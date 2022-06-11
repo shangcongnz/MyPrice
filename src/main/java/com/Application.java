91d8701c83a5f80298e0b1620af4fea6693dce4f
shang <shang@Cong-P15V>
2022-06-12 00:11:49 +1200

    start a new phase

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -12,7 +12,7 @@ public class Application {
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
-        System.out.println("*** ***********************************  ****");
+        System.out.println("******************************************     ");
 
 
     }
